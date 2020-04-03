f4d65769523c99df49b1899f391e20811139072b
Neil Denton <ndenton@lionel.com>
2020-04-03 10:41:21 -0400

    Added Common Modules to profile Script.

--- profile.ps1
+++ profile.ps1
@@ -1,1 +1,7 @@
+#Import Common Modules
+Import-Module ActiveDirectory
+Import-Module $((Get-ChildItem -Path $($env:LOCALAPPDATA+"\Apps\2.0\") -Filter CreateExoPSSession.ps1 -Recurse ).FullName | Select-Object -Last 1)
+Import-Module MSOnline
+
+#Load Functions
 Get-ChildItem $HOME\Documents\PowerShell\Functions\*Function.ps1 | %{. $_ }
